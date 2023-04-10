d1d92f3ec24885869f791137d8707082f64efa85
thalyasb <thalya.tec@gmail.com>
2023-04-10 10:03:58 -0500

    Persistindo dados no banco de dados

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,7 +4,33 @@
 
 @section('content')
 
-<h1>Crie um Evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie seu Evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
+        </div>
+        <div class="form-group">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do Evento">
+        </div>
+        <div class="form-group">
+            <label for="title">O evento é privado?</label>
+            <select class="form-control" name="private" id="private">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="title">Descrição do Evento:</label>
+          <textarea class="form-control" name="description" id="description" placeholder="O que vai acontecer no evento "></textarea>
+        </div>
+        <br>
+        <input type="submit" class="btn btn-primary" value="Criar Evento">
+    </form>
+</div>
     
 @endsection
         
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,10 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/produtos', function () {
-
-    $busca = request('search');
-    
-
-    return view('produtos', ['busca' => $busca]);
+    return view('produtos');
 });
 
-Route::get('/produtos/{id}', function ($id) {
-    return view('produto', ['id' => $id]);
-});
+
