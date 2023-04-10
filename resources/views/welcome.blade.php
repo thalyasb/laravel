374f71bb247cab36b1653039d543bf0d0939251c
thalyasb <thalya.tec@gmail.com>
2023-04-10 09:09:51 -0500

    Estilizando a pagina home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,23 @@
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
-    
+<div id="events-container" class="cold-md-12">
+    <h2>Próximo Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach ($events as $events)
+            <div class="card col-md-3">
+                <img src="/img/banner.jpeg" alt="{{$events->title}}">
+                <div class="card-body">
+                    <p class="card-date">10/04/2023</p>
+                    <h5 class="card-title">{{$events->title}}</h5>
+                    <p class="card-participants">X Participantes</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
         
         
